7529eb096ead87f909b6eea2c85928412c6ce5f8
krutaxe <krutaxe@mail.ru>
2022-11-06 20:16:47 +0300

    6

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,5 @@
 package org.example;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
